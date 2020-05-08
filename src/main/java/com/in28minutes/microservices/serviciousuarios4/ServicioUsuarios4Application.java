ed31ae5cec4bf2876ac9d7f4db38a6d845f98696
amunozpe@vectoritcgroup.com <amunozpe@vectoritcgroup.com>
2020-05-08 20:18:32 +0200

    firs commit

---
+++ src/main/java/com/in28minutes/microservices/serviciousuarios4/ServicioUsuarios4Application.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.serviciousuarios4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioUsuarios4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioUsuarios4Application.class, args);
+	}
+
+}
